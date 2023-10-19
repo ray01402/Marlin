fcbdaf950c399e2484fe27e843912634dfa42ea7
Ray014 <reenoo.ahmed@gmail.com>
2023-10-19 17:07:53 +0100

    2.1.2R1.4

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -25,7 +25,7 @@
  * Release version. Leave the Marlin version or apply a custom scheme.
  */
 #ifndef SHORT_BUILD_VERSION
-  #define SHORT_BUILD_VERSION "2.1.2R1.3"
+  #define SHORT_BUILD_VERSION "2.1.2R1.4"
 #endif
 
 /**
